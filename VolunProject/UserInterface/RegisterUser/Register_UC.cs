4496df20f3803de528c32b1aebafd9040e808df4
VANTUANKIET1239 <vantuankiet2207@gmail.com>
2023-12-03 19:47:31 +0700

    day len 3

---
+++ VolunProject/UserInterface/RegisterUser/Register_UC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolunProject.UserInterface.RegisterUser
+{
+    public partial class Register_UC : UserControl
+    {
+        public Register_UC()
+        {
+            InitializeComponent();
+        }
+    }
+}
