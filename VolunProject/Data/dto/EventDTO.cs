7c8256965b8d9242386ac871ae44fbcf39a21f1a
VANTUANKIET1239 <vantuankiet2207@gmail.com>
2023-12-12 22:39:55 +0700

    kiet day 12/12/2023

---
+++ VolunProject/Data/dto/EventDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolunProject.Data.DTO
+{
+    public class EventDTO
+    {
+        public string EventID { get; set; }
+        public string EventName { get; set; }
+        public int DistrictId { get; set; }
+        public int CityId { get; set; }
+        public int WardId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; }
+        public string CategoryId { get; set; }
+        public bool state { get; set; }
+        public string description { get; set; }
+        public string purpose { get; set; }
+        public string time { get; set; }
+        public byte[] EventImage { get; set; }
+    }
+}
